5d809035e849c1cf670c86c2acfe75f4e7acf1ea
Ignacy Klimont <98184936+ignacyklimont@users.noreply.github.com>
2022-07-20 19:44:01 +0200

    Delete Topics directory

--- Topics/Math functions/Sine and cosine/main.py
+++
@@ -1,5 +0,0 @@
-import math
-
-rad = float(input())
-result = math.sin(rad) - math.cos(rad)
-print(result)
--- Topics/Math functions/The area of a circle/main.py
+++
@@ -1,5 +0,0 @@
-import math
-
-rad = float(input())
-area = math.pow(rad,2)*math.pi
-print(round(area,2))
